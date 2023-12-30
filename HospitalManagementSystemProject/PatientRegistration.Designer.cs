ec92778db58f97aa26e4110e58d7718dcd302f82
Enes Balci <enesbalci011@gmail.com>
2023-12-30 22:21:16 +0300

    Enes project  second commit done.

--- HospitalManagementSystemProject/PatientRegistration.Designer.cs
+++ HospitalManagementSystemProject/PatientRegistration.Designer.cs
@@ -49,37 +49,41 @@
             button1.BackColor = Color.FromArgb(0, 192, 0);
             button1.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
             button1.ForeColor = SystemColors.ActiveCaptionText;
-            button1.Location = new Point(333, 448);
+            button1.Location = new Point(381, 597);
+            button1.Margin = new Padding(3, 4, 3, 4);
             button1.Name = "button1";
-            button1.Size = new Size(171, 47);
+            button1.Size = new Size(195, 63);
             button1.TabIndex = 12;
             button1.Text = "Kaydet";
             button1.UseVisualStyleBackColor = false;
             // 
             // textBox1
             // 
+            textBox1.BackColor = Color.White;
             textBox1.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            textBox1.Location = new Point(262, 104);
+            textBox1.Location = new Point(299, 139);
+            textBox1.Margin = new Padding(3, 4, 3, 4);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(312, 34);
+            textBox1.Size = new Size(356, 41);
             textBox1.TabIndex = 11;
             // 
             // maskedTextBox1
             // 
             maskedTextBox1.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            maskedTextBox1.Location = new Point(262, 214);
+            maskedTextBox1.Location = new Point(299, 288);
+            maskedTextBox1.Margin = new Padding(3, 4, 3, 4);
             maskedTextBox1.Mask = "00000000000";
             maskedTextBox1.Name = "maskedTextBox1";
-            maskedTextBox1.Size = new Size(312, 35);
+            maskedTextBox1.Size = new Size(356, 42);
             maskedTextBox1.TabIndex = 10;
             // 
             // label3
             // 
             label3.AutoSize = true;
             label3.Font = new Font("Corbel", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label3.Location = new Point(155, 161);
+            label3.Location = new Point(177, 215);
             label3.Name = "label3";
-            label3.Size = new Size(101, 33);
+            label3.Size = new Size(129, 41);
             label3.TabIndex = 9;
             label3.Text = "Soyad : ";
             // 
@@ -87,9 +91,9 @@
             // 
             label2.AutoSize = true;
             label2.Font = new Font("Corbel", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label2.Location = new Point(193, 105);
+            label2.Location = new Point(221, 140);
             label2.Name = "label2";
-            label2.Size = new Size(63, 33);
+            label2.Size = new Size(81, 41);
             label2.TabIndex = 8;
             label2.Text = "Ad : ";
             // 
@@ -98,9 +102,9 @@
             label1.AutoSize = true;
             label1.Font = new Font("Corbel", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
             label1.ForeColor = SystemColors.Highlight;
-            label1.Location = new Point(310, 25);
+            label1.Location = new Point(354, 33);
             label1.Name = "label1";
-            label1.Size = new Size(216, 33);
+            label1.Size = new Size(279, 41);
             label1.TabIndex = 7;
             label1.Text = "Hasta Kayıt Ekranı";
             // 
@@ -108,9 +112,9 @@
             // 
             label4.AutoSize = true;
             label4.Font = new Font("Corbel", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label4.Location = new Point(82, 217);
+            label4.Location = new Point(77, 289);
             label4.Name = "label4";
-            label4.Size = new Size(174, 33);
+            label4.Size = new Size(225, 41);
             label4.TabIndex = 14;
             label4.Text = "TC Kimlik No : ";
             // 
@@ -118,9 +122,9 @@
             // 
             label5.AutoSize = true;
             label5.Font = new Font("Corbel", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label5.Location = new Point(142, 273);
+            label5.Location = new Point(162, 364);
             label5.Name = "label5";
-            label5.Size = new Size(114, 33);
+            label5.Size = new Size(147, 41);
             label5.TabIndex = 15;
             label5.Text = "Telefon : ";
             // 
@@ -128,9 +132,9 @@
             // 
             label6.AutoSize = true;
             label6.Font = new Font("Corbel", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label6.Location = new Point(136, 329);
+            label6.Location = new Point(155, 439);
             label6.Name = "label6";
-            label6.Size = new Size(120, 33);
+            label6.Size = new Size(154, 41);
             label6.TabIndex = 16;
             label6.Text = "Cinsiyet : ";
             // 
@@ -138,36 +142,39 @@
             // 
             label7.AutoSize = true;
             label7.Font = new Font("Corbel", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label7.Location = new Point(177, 385);
+            label7.Location = new Point(202, 513);
             label7.Name = "label7";
-            label7.Size = new Size(79, 33);
+            label7.Size = new Size(100, 41);
             label7.TabIndex = 17;
             label7.Text = "Şifre :";
             // 
             // textBox2
             // 
             textBox2.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            textBox2.Location = new Point(262, 159);
+            textBox2.Location = new Point(299, 212);
+            textBox2.Margin = new Padding(3, 4, 3, 4);
             textBox2.Name = "textBox2";
-            textBox2.Size = new Size(312, 34);
+            textBox2.Size = new Size(356, 41);
             textBox2.TabIndex = 18;
             // 
             // textBox3
             // 
             textBox3.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            textBox3.Location = new Point(262, 383);
+            textBox3.Location = new Point(299, 511);
+            textBox3.Margin = new Padding(3, 4, 3, 4);
             textBox3.Name = "textBox3";
-            textBox3.Size = new Size(312, 34);
+            textBox3.Size = new Size(356, 41);
             textBox3.TabIndex = 19;
             textBox3.UseSystemPasswordChar = true;
             // 
             // maskedTextBox2
             // 
             maskedTextBox2.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            maskedTextBox2.Location = new Point(262, 270);
+            maskedTextBox2.Location = new Point(299, 360);
+            maskedTextBox2.Margin = new Padding(3, 4, 3, 4);
             maskedTextBox2.Mask = "(999) 000-0000";
             maskedTextBox2.Name = "maskedTextBox2";
-            maskedTextBox2.Size = new Size(312, 35);
+            maskedTextBox2.Size = new Size(356, 42);
             maskedTextBox2.TabIndex = 20;
             // 
             // comboBox1
@@ -175,17 +182,18 @@
             comboBox1.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point, 0);
             comboBox1.FormattingEnabled = true;
             comboBox1.Items.AddRange(new object[] { "Erkek", "Kadın" });
-            comboBox1.Location = new Point(262, 326);
+            comboBox1.Location = new Point(299, 435);
+            comboBox1.Margin = new Padding(3, 4, 3, 4);
             comboBox1.Name = "comboBox1";
-            comboBox1.Size = new Size(312, 36);
+            comboBox1.Size = new Size(356, 43);
             comboBox1.TabIndex = 21;
             // 
             // PatientRegistration
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            BackColor = SystemColors.GradientActiveCaption;
-            ClientSize = new Size(797, 536);
+            BackColor = Color.FromArgb(0, 192, 192);
+            ClientSize = new Size(911, 715);
             Controls.Add(comboBox1);
             Controls.Add(maskedTextBox2);
             Controls.Add(textBox3);
@@ -201,6 +209,7 @@
             Controls.Add(label2);
             Controls.Add(label1);
             FormBorderStyle = FormBorderStyle.FixedDialog;
+            Margin = new Padding(3, 4, 3, 4);
             Name = "PatientRegistration";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Hasta Kayıt ";
