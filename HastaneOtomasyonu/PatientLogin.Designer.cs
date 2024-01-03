74dfc9b342b2ea6991eb11181f885bbedfae154a
Enes Balci <enesbalci011@gmail.com>
2024-01-03 18:51:18 +0300

    Versionenes Project Four Section Completed

--- HastaneOtomasyonu/PatientLogin.Designer.cs
+++ HastaneOtomasyonu/PatientLogin.Designer.cs
@@ -41,7 +41,7 @@
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe Script", 24F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label1.Location = new Point(58, 24);
+            label1.Location = new Point(56, 26);
             label1.Name = "label1";
             label1.Size = new Size(419, 67);
             label1.TabIndex = 0;
