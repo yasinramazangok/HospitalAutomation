f1c953c2bfe7f12530f13f29f269a72629d76836
Enes Balci <enesbalci011@gmail.com>
2024-01-03 17:57:08 +0300

    Versionenes Project Three Section Completed

--- HastaneOtomasyonu/Patient detail.Designer.cs
+++ HastaneOtomasyonu/Patient detail.Designer.cs
@@ -29,21 +29,30 @@
         private void InitializeComponent()
         {
             groupBox1 = new GroupBox();
+            button2 = new Button();
+            label4 = new Label();
+            label3 = new Label();
             label2 = new Label();
             label1 = new Label();
-            label3 = new Label();
-            label4 = new Label();
             groupBox2 = new GroupBox();
-            comboBox1 = new ComboBox();
-            richTextBox1 = new RichTextBox();
-            button2 = new Button();
-            label5 = new Label();
-            comboBox2 = new ComboBox();
-            label6 = new Label();
             button1 = new Button();
             label7 = new Label();
+            richTextBox1 = new RichTextBox();
+            label6 = new Label();
+            label5 = new Label();
+            comboBox2 = new ComboBox();
+            comboBox1 = new ComboBox();
+            groupBox3 = new GroupBox();
+            groupBox4 = new GroupBox();
+            dataGridView1 = new DataGridView();
+            dataGridView2 = new DataGridView();
+            linkLabel1 = new LinkLabel();
             groupBox1.SuspendLayout();
             groupBox2.SuspendLayout();
+            groupBox3.SuspendLayout();
+            groupBox4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).BeginInit();
             SuspendLayout();
             // 
             // groupBox1
@@ -55,10 +64,41 @@
             groupBox1.Controls.Add(label1);
             groupBox1.Location = new Point(12, 12);
             groupBox1.Name = "groupBox1";
-            groupBox1.Size = new Size(361, 207);
+            groupBox1.Size = new Size(441, 207);
             groupBox1.TabIndex = 0;
             groupBox1.TabStop = false;
             groupBox1.Text = "Kişi Bilgi";
+            // 
+            // button2
+            // 
+            button2.Location = new Point(749, 304);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 5;
+            button2.Text = "button1";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 162);
+            label4.Location = new Point(178, 118);
+            label4.Margin = new Padding(5, 0, 5, 0);
+            label4.Name = "label4";
+            label4.Size = new Size(104, 31);
+            label4.TabIndex = 7;
+            label4.Text = "Null Null";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 162);
+            label3.Location = new Point(178, 53);
+            label3.Margin = new Padding(5, 0, 5, 0);
+            label3.Name = "label3";
+            label3.Size = new Size(146, 31);
+            label3.TabIndex = 6;
+            label3.Text = "00000000000";
             // 
             // label2
             // 
@@ -82,30 +122,9 @@
             label1.TabIndex = 6;
             label1.Text = "Ad Soyad:";
             // 
-            // label3
-            // 
-            label3.AutoSize = true;
-            label3.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 162);
-            label3.Location = new Point(178, 53);
-            label3.Margin = new Padding(5, 0, 5, 0);
-            label3.Name = "label3";
-            label3.Size = new Size(146, 31);
-            label3.TabIndex = 6;
-            label3.Text = "00000000000";
-            // 
-            // label4
-            // 
-            label4.AutoSize = true;
-            label4.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 162);
-            label4.Location = new Point(178, 118);
-            label4.Margin = new Padding(5, 0, 5, 0);
-            label4.Name = "label4";
-            label4.Size = new Size(104, 31);
-            label4.TabIndex = 7;
-            label4.Text = "Null Null";
-            // 
             // groupBox2
             // 
+            groupBox2.Controls.Add(linkLabel1);
             groupBox2.Controls.Add(button1);
             groupBox2.Controls.Add(label7);
             groupBox2.Controls.Add(richTextBox1);
@@ -113,68 +132,13 @@
             groupBox2.Controls.Add(label5);
             groupBox2.Controls.Add(comboBox2);
             groupBox2.Controls.Add(comboBox1);
-            groupBox2.Location = new Point(12, 255);
+            groupBox2.Location = new Point(12, 225);
             groupBox2.Name = "groupBox2";
-            groupBox2.Size = new Size(361, 391);
+            groupBox2.Size = new Size(441, 421);
             groupBox2.TabIndex = 1;
             groupBox2.TabStop = false;
             groupBox2.Text = "Randevu Paneli";
             groupBox2.Enter += groupBox2_Enter;
-            // 
-            // comboBox1
-            // 
-            comboBox1.FormattingEnabled = true;
-            comboBox1.Location = new Point(116, 80);
-            comboBox1.Name = "comboBox1";
-            comboBox1.Size = new Size(184, 28);
-            comboBox1.TabIndex = 2;
-            // 
-            // richTextBox1
-            // 
-            richTextBox1.Location = new Point(116, 175);
-            richTextBox1.Name = "richTextBox1";
-            richTextBox1.Size = new Size(184, 130);
-            richTextBox1.TabIndex = 4;
-            richTextBox1.Text = "";
-            // 
-            // button2
-            // 
-            button2.Location = new Point(749, 304);
-            button2.Name = "button2";
-            button2.Size = new Size(94, 29);
-            button2.TabIndex = 5;
-            button2.Text = "button1";
-            button2.UseVisualStyleBackColor = true;
-            // 
-            // label5
-            // 
-            label5.AutoSize = true;
-            label5.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 162);
-            label5.Location = new Point(33, 74);
-            label5.Margin = new Padding(5, 0, 5, 0);
-            label5.Name = "label5";
-            label5.Size = new Size(75, 31);
-            label5.TabIndex = 6;
-            label5.Text = "Branş:";
-            // 
-            // comboBox2
-            // 
-            comboBox2.FormattingEnabled = true;
-            comboBox2.Location = new Point(116, 126);
-            comboBox2.Name = "comboBox2";
-            comboBox2.Size = new Size(184, 28);
-            comboBox2.TabIndex = 2;
-            // 
-            // label6
-            // 
-            label6.AutoSize = true;
-            label6.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 162);
-            label6.Location = new Point(20, 124);
-            label6.Margin = new Padding(5, 0, 5, 0);
-            label6.Name = "label6";
-            label6.Size = new Size(88, 31);
-            label6.TabIndex = 6;
-            label6.Text = "Doktor:";
             // 
             // button1
             // 
@@ -199,12 +163,110 @@
             label7.TabIndex = 6;
             label7.Text = "Şİkayet:";
             // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(116, 175);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(184, 130);
+            richTextBox1.TabIndex = 4;
+            richTextBox1.Text = "";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 162);
+            label6.Location = new Point(20, 124);
+            label6.Margin = new Padding(5, 0, 5, 0);
+            label6.Name = "label6";
+            label6.Size = new Size(88, 31);
+            label6.TabIndex = 6;
+            label6.Text = "Doktor:";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 162);
+            label5.Location = new Point(33, 74);
+            label5.Margin = new Padding(5, 0, 5, 0);
+            label5.Name = "label5";
+            label5.Size = new Size(75, 31);
+            label5.TabIndex = 6;
+            label5.Text = "Branş:";
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(116, 126);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(184, 28);
+            comboBox2.TabIndex = 2;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(116, 80);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(184, 28);
+            comboBox1.TabIndex = 2;
+            // 
+            // groupBox3
+            // 
+            groupBox3.Controls.Add(dataGridView1);
+            groupBox3.Location = new Point(475, 12);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new Size(664, 257);
+            groupBox3.TabIndex = 2;
+            groupBox3.TabStop = false;
+            groupBox3.Text = "Randevu Geçmişi";
+            // 
+            // groupBox4
+            // 
+            groupBox4.Controls.Add(dataGridView2);
+            groupBox4.Location = new Point(475, 288);
+            groupBox4.Name = "groupBox4";
+            groupBox4.Size = new Size(664, 358);
+            groupBox4.TabIndex = 2;
+            groupBox4.TabStop = false;
+            groupBox4.Text = "Aktif Randevular";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(6, 19);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(652, 232);
+            dataGridView1.TabIndex = 0;
+            // 
+            // dataGridView2
+            // 
+            dataGridView2.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView2.Location = new Point(6, 17);
+            dataGridView2.Name = "dataGridView2";
+            dataGridView2.RowHeadersWidth = 51;
+            dataGridView2.Size = new Size(652, 335);
+            dataGridView2.TabIndex = 0;
+            // 
+            // linkLabel1
+            // 
+            linkLabel1.AutoSize = true;
+            linkLabel1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 162);
+            linkLabel1.LinkColor = Color.Blue;
+            linkLabel1.Location = new Point(6, 387);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new Size(152, 28);
+            linkLabel1.TabIndex = 11;
+            linkLabel1.TabStop = true;
+            linkLabel1.Text = "Bilgileri Düzenle";
+            // 
             // Patient_detail
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.Coral;
             ClientSize = new Size(1151, 658);
+            Controls.Add(groupBox4);
+            Controls.Add(groupBox3);
             Controls.Add(groupBox2);
             Controls.Add(groupBox1);
             FormBorderStyle = FormBorderStyle.FixedDialog;
@@ -215,6 +277,10 @@
             groupBox1.PerformLayout();
             groupBox2.ResumeLayout(false);
             groupBox2.PerformLayout();
+            groupBox3.ResumeLayout(false);
+            groupBox4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).EndInit();
             ResumeLayout(false);
         }
 
@@ -234,5 +300,10 @@
         private ComboBox comboBox2;
         private Button button1;
         private Label label7;
+        private GroupBox groupBox3;
+        private DataGridView dataGridView1;
+        private GroupBox groupBox4;
+        private DataGridView dataGridView2;
+        private LinkLabel linkLabel1;
     }
 }
