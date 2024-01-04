26822e7cd70909744525ef09ae63df129e999ff3
Enes Balci <enesbalci011@gmail.com>
2024-01-05 00:44:01 +0300

    versionenes Project six section completed

---
+++ HastaneOtomasyonu/SqlBağlantısı.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+
+namespace HastaneOtomasyonu
+{
+     class SqlBağlantısı
+    {
+
+
+        public  SqlBağlantısı () 
+        {
+
+            SqlBağlantısı baglantı =new SqlBağlantısı();
+        }
+    }
+
+    
+}
